f995395585aa0179bb0b99204abb8b84b138697b
igorforspeak@gmail.com <igorforspeak@gmail.com>
2023-06-20 13:19:14 +0300

    upd templates, add form

--- landing_project/product_app/views.py
+++ landing_project/product_app/views.py
@@ -5,3 +5,8 @@ from django.template import loader
 def index(request):
     template = loader.get_template('index.html')
     return HttpResponse(template.render())
+
+
+def form(request):
+    template = loader.get_template('form.html')
+    return HttpResponse(template.render())
