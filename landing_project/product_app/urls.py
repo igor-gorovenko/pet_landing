dc37d3cb077d200219de6f752e38cbcd20ab5f20
igorforspeak@gmail.com <igorforspeak@gmail.com>
2023-06-23 12:10:56 +0300

    add form login and sign up

--- landing_project/product_app/urls.py
+++ landing_project/product_app/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('form/', views.form, name='form'),
+    path('signup/', views.SignUp.as_view(), name='signup'),
 ]
--- landing_project/product_app/views.py
+++ landing_project/product_app/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import get_object_or_404, render, HttpResponse
+from django.shortcuts import render, HttpResponse
 from django.template import loader
+from django.urls import reverse_lazy
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.edit import CreateView
 
 from .models import Product, Advantage
 
@@ -17,3 +20,12 @@ def index(request):
 def form(request):
     template = loader.get_template('form.html')
     return HttpResponse(template.render())
+
+
+class SignUp(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = "registration/signup.html"
+
+    
+    
